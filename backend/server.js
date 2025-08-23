fff1606d950371d3f74d5f8b262e04408dbab117
Murilo Augusto <140767549+Muraups@users.noreply.github.com>
2025-08-23 01:08:48 -0300

    Testes api no vercel

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,32 @@
 // backend/server.js
-import path from 'path'; 
-import { fileURLToPath } from 'url'; 
-import express from 'express';
-//import sequelize from './config/database.js';
-import servicoRoutes from './routes/servicoRoutes.js';
-import clienteRoutes from './routes/clienteRoutes.js'; // Importe as novas rotas
-import { sequelize } from './models/index.js'; // Importa para iniciar a conexão
-import cors from 'cors';
 
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import servicoRoutes from "./routes/servicoRoutes.js";
+import clienteRoutes from "./routes/clienteRoutes.js";
+import { sequelize } from "./models/index.js";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 app.use(cors());
 app.use(express.json());
-app.use('/api', servicoRoutes);
-app.use('/api', clienteRoutes);
+app.use("/api", servicoRoutes);
+app.use("/api", clienteRoutes);
 
-app.use(express.static(path.join(__dirname, '../frontend/html'))); // Para servir os HTMLs diretamente
-app.use('/css', express.static(path.join(__dirname, '../frontend/css'))); // Para servir CSS
-app.use('/js', express.static(path.join(__dirname, '../frontend/js')));   // Para servir JS
+app.use(express.static(path.join(__dirname, "../frontend/html")));
+app.use("/css", express.static(path.join(__dirname, "../frontend/css")));
+app.use("/js", express.static(path.join(__dirname, "../frontend/js")));
 
-
+// inicia conexão com banco
 sequelize.sync().then(() => {
-  console.log('🟢 Banco sincronizado');
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-  });
+  console.log("🟢 Banco sincronizado");
 }).catch((err) => {
-  console.error('❌ Erro ao conectar ao banco:', err);
+  console.error("❌ Erro ao conectar ao banco:", err);
 });
+
+// Exporta para o Vercel usar
+export default app;
